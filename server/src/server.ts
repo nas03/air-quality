0d6920e571a940d9210428a8cc75042c7e97de1d
nas03 <sonanhnguyen003@gmail.com>
2024-12-03 18:46:16 +0700

    fix:

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import routes from "@/api/routes";
 import cors from "cors";
 import { config } from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
 
 config();
 
@@ -12,6 +12,9 @@ server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use(cors());
 server.use("/api", routes);
+server.use("/", (req: Request, res: Response) => {
+  res.send("Welcome to AirQ");
+});
 server.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
 });
