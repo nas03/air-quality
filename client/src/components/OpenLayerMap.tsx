02d6186230e65800c898a7af33d9c904fa90483e
nas03 <sonanhnguyen003@gmail.com>
2025-01-04 04:31:27 +0700

    update: render map as vector

--- client/src/components/OpenLayerMap.tsx
+++ client/src/components/OpenLayerMap.tsx
@@ -1,69 +1,77 @@
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
-import { OSM, TileWMS } from "ol/source";
+import { TileWMS } from "ol/source";
 import "ol/ol.css";
 import "@/open.css";
 import { useEffect, useRef } from "react";
+import { apply } from "ol-mapbox-style";
+import { fromLonLat } from "ol/proj";
+
 interface IPropsOpenLayerMap {
   time: string;
 }
 const OpenLayerMap: React.FC<IPropsOpenLayerMap> = ({ time }) => {
-  const wmsLayerRef = useRef<TileLayer<TileWMS> | null>(null);
+  const layersRef = useRef<TileLayer[] | null>(null);
+
   useEffect(() => {
+    const layers = [
+      new TileLayer({
+        source: new TileWMS({
+          url: "http://localhost:8080/geoserver/air/wms",
+          params: {
+            LAYERS: "air:AQI",
+            TIME: time,
+            TILED: true,
+          },
+          serverType: "geoserver",
+          cacheSize: 4096,
+          crossOrigin: "anonymous",
+        }),
+        opacity: 0.6,
+      }),
+      new TileLayer({
+        source: new TileWMS({
+          url: "http://localhost:8080/geoserver/air/wms",
+          params: {
+            LAYERS: "air:gadm41_VNM_2",
+            TILED: true,
+          },
+          serverType: "geoserver",
+          cacheSize: 4096,
+          crossOrigin: "anonymous",
+        }),
+        opacity: 0.4,
+        maxZoom: 12,
+      }),
+    ];
+    layersRef.current = layers;
+
+    const key = "Y2LLlbvRVwaYxT2YxjWV";
+    const styleUrl = `https://api.maptiler.com/maps/7d9ee8e1-7abf-4591-ac75-85518e48ba38/style.json?key=${key}`;
+
     const map = new Map({
       target: "map",
-      layers: [
-        new TileLayer({
-          source: new OSM({
-            url: "https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=Y2LLlbvRVwaYxT2YxjWV",
-            attributions:
-              '\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
-          }),
-        }),
-      ],
-      view: new View({
-        center: [105.97, 17.9459],
-        projection: "EPSG:4326",
-        zoom: 8,
-      }),
-    });
 
-    const wmsLayer = new TileLayer({
-      source: new TileWMS({
-        url: "http://localhost:8080/geoserver/air/wms",
-        projection: "EPSG:4326",
-        params: {
-          LAYERS: "air:AQI",
-          TIME: time,
-          TILED: true,
-        },
-        serverType: "geoserver",
+      view: new View({
+        zoom: 8,
+        constrainResolution: true,
+        projection: "EPSG:3857",
+        center: fromLonLat([105.97, 17.9459]),
       }),
-      opacity: 0.6,
     });
-    map.addLayer(wmsLayer);
-    const labelLayer = new TileLayer({
-      source: new TileWMS({
-        url: "http://localhost:8080/geoserver/air/wms",
-        projection: "EPSG:4326",
-        params: {
-          LAYERS: "air:gadm41_VNM_2",
-          TILED: true,
-        },
-        serverType: "geoserver",
-      }),
-      opacity: 0.7,
-      maxZoom: 12,
-    });
-    map.addLayer(labelLayer);
-    wmsLayerRef.current = wmsLayer;
+    Promise.resolve(
+      apply(map, styleUrl).then(() => map.getLayers().extend([...layers])),
+    );
+
     return () => {
       map.dispose();
     };
   }, []);
 
   useEffect(() => {
-    wmsLayerRef.current?.getSource()?.updateParams({ TIME: time });
+    (layersRef.current?.at(0)?.getSource() as TileWMS).updateParams({
+      TIME: time,
+    });
   }, [time]);
   return (
     <>
