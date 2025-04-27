18a6317f5ae09a62655186eec14e0fc5bd5d0554
nas03 <sonanhnguyen003@gmail.com>
2025-04-28 05:58:46 +0700

    update: https

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,7 +16,6 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 server.use(morgan("dev"));
-
 server.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
