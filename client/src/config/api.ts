afa2f40f7fb7d917bb760e40f80074a3848e0274
nas03 <sonanhnguyen003@gmail.com>
2025-04-27 17:20:01 +0700

    fix(api): update baseURL to production server

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:5500/api",
+    baseURL: "http://api.nas03.xyz:5500/api",
 
     // baseURL: "http://ec2-18-136-124-168.ap-southeast-1.compute.amazonaws.com:5500/api",
 });
